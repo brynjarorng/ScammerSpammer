39e29bb57a194dfe311eaca22f1336766ceeb33a
carlos-ventura <carlospinho_pt@hotmail.com>
2021-12-01 17:12:43 +0100

    Fast update on script to run it

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ScammerSpammer
-A simple script that uses selenium to input random pseudo realistic data into a form. Should not be used for evil, only ment for testing
+A simple script that uses selenium to input random pseudo realistic data into a form. Should not be used for evil, only meant for testing :)
--- spammers.py
+++ spammers.py
@@ -16,25 +16,36 @@ with open("passwords.csv", newline='') as f:
     for l in csv.reader(f, delimiter=','):
         passwords.append(l[0])
 
-address = "http://support-danmarkstekniskeuniversitet.moonfruit.com/"
+
+address = "https://supportdanmarkstekniskeuniversitet.moonfruit.com/"
 post_address = "https://secure.sitemakerlive.com/_form/submit"
 
-browser = webdriver.Chrome(executable_path="/home/brynjarog/selenium_chrome/drivers/chromedriver")
+
 
 # Fields to target
-name_field = "BJAgcCurdAH"
-email_field = "rykWqA_B_CS"
-password_field = "Bk2eYBOCH"
-alternative_email_field = "HyFltSuAr"
-alternative_password_field = "SkoclRmKF"
-birthday_field = "Bycnl0mtK"
+name_field = "ByAg5riaXYK"
+email_field = "SJJW9HopXYt"
+dtu_username = "rJIBQDoamFF"
+password_field = "rkQwiTmYt"
+alternative_email_field = "SkMvjT7Kt"
+alternative_password_field = "r1-vs6mKF"
+birthday_field = "r1KLjp7Kt"
+state = "rywIo6mYF"
+
+button= "/html/body/div[1]/div[1]/section/div/div[2]/div/form/div[10]/button"
+
+state_options = ["student", "phd", "masters", "bachelor", "part-time", "full-time", "working and studying", "studying", "employer", "Spain", "Portugal", "Greece", "Italy", "Denmark", 
+"Naerum", "Copenhagen", "Lisbon", "Sydney", "Alasca", "Alequente", "Buenos-Aires", "Borussia", "Germany", "Lyngby", "single", "married", "Single", "Married", "Engaged", "engaged"]
 
 
-for i in range(3):
+
+for i in range(100000000):
+    browser = webdriver.Chrome()
     browser.get(address)
 
     # Chose a name, generate an email for dtu as well as a random username
     f_name = random.choice(names)
+    m_name =str(random.choice([ " " + str(random.choice(names)), "", "", ""]))
     l_name = random.choice(names)
     s_id = "s" + random.choice(["19", "20", "21"]) + str(random.randrange(1000,9999))
 
@@ -50,17 +61,18 @@ for i in range(3):
 
 
 
-    # browser.find_element(By.NAME, name_field).send_keys(f_name + " " + l_name)
-    # browser.find_element(By.NAME, email_field).send_keys(s_id + "@student.dtu.dk")
-    # browser.find_element(By.NAME, password_field).send_keys(random.choice(passwords))
-    # browser.find_element(By.NAME, alternative_email_fielde_field).send_keys(f'{fname.lower()}{random.randrange(0, 99)}@{random.choice(["gmail.com", "outlook.com", "hotmail.com", "live.com", "sapo.pt"])}')
-    # browser.find_element(By.NAME, alternative_password_field).send_keys(random.choice(passwords))
-    # browser.find_element(By.NAME, birthday_field).send_keys(str(random_date))
+    browser.find_element(By.NAME, name_field).send_keys(f_name + m_name + " " + l_name)
+    browser.find_element(By.NAME, email_field).send_keys(s_id + "@student.dtu.dk")
+    browser.find_element(By.NAME, dtu_username).send_keys(s_id)
+    browser.find_element(By.NAME, password_field).send_keys(random.choice(passwords))
+    browser.find_element(By.NAME, alternative_email_field).send_keys(f'{f_name.lower()}{random.randrange(0, 99)}@{random.choice(["gmail.com", "outlook.com", "hotmail.com", "live.com", "sapo.pt"])}')
+    browser.find_element(By.NAME, alternative_password_field).send_keys(random.choice(passwords))
+    browser.find_element(By.NAME, birthday_field).send_keys(str(random_date))
+    browser.find_element(By.NAME, state).send_keys(random.choice(state_options))
 
+    browser.find_element(By.XPATH, button).click()
     # TODO: add post
 
-
-    time.sleep(1)
-
+    time.sleep(2)
 
 browser.quit()
